f10c2ed158eb41733152ded0bd7b99b6a9109d5b
Lucía Costamagna <luci3220@hotmail.com>
2023-06-30 22:21:08 -0300

    add fix dashBoard

--- src/Screen/DashboardScreen.tsx
+++ src/Screen/DashboardScreen.tsx
@@ -6,21 +6,36 @@ import {
   Dimensions,
   Image,
   Text,
-  ImageBackground 
+  ImageBackground,
+  TouchableOpacity,
 } from "react-native";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const DashboardScreen = () => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require("../../assets/Arg.jpeg")} style={styles.imageBackground}>
-      
-        <Text style={styles.text}>ARGENTINA</Text>
-        <View style={{width:'70%', marginRight:'20%'}}>
-        <Text style={styles.text1}> Desde la vibrante vida urbana hasta la serenidad de sus paisajes naturales, te espera un viaje inolvidable.</Text>
+      <ImageBackground
+        source={require("../../assets/Arg.jpeg")}
+        style={styles.imageBackground}
+      >
+         <View style={styles.ViewIcon}>
+          <Ionicons name="location-outline" size={45} color="black" />
         </View>
-        <View style={styles.ViewIcon}>
-        <Ionicons name="location-outline" size={45} color="black" />
+        <Text style={styles.text}>ARGENTINA</Text>
+        <View style={{ width: "70%", marginRight: "20%" }}>
+          <Text style={styles.text1}>
+            {" "}
+            Desde la vibrante vida urbana hasta la serenidad de sus paisajes
+            naturales, te espera un viaje inolvidable.
+          </Text>
+        </View>
+       
+        <View style={styles.ViewBotton}>
+<Text style={styles.text2}> COMENZAR</Text>
+          <TouchableOpacity style={styles.button} activeOpacity={0.8}> 
+        
+          <Ionicons name="chevron-forward-outline" size={35} color="black" marginLeft={25} marginTop={7} />
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
@@ -30,31 +45,55 @@ const DashboardScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageBackground: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 38,
-    fontWeight: 'bold',
-    color: 'black',
-   marginTop:'-100%',
-   marginRight:'25%'
+    fontWeight: "bold",
+    color: "black",
+    marginTop: "2%",
+    marginRight: "25%",
   },
-  text1:{
+  text1: {
     fontSize: 13,
-         textAlign: 'justify',
-         color:'black'
+    textAlign: "justify",
+    color: "black",
   },
-  ViewIcon:{
-    marginTop:'-38%',
-    marginRight:'65%'
+  ViewIcon: {
+    marginTop: "3%",
+    marginRight: "65%",
+  },
+  ViewBotton:{
+width:'80%',
+height:'8%',
+borderRadius:30,
+backgroundColor:'#717D7E',
+marginTop:'100%',
+
+  },
+  button:{
+width:'23%',
+height:'80%',
+borderRadius:30,
+backgroundColor:'#48C9B0',
+marginLeft:'75%',
+marginTop:'-9%'
+  },
+  text2:{
+    color:'black',
+    marginLeft:'30%',
+    marginTop:'5%',
+    fontSize: 15,
+    
+
   }
 });
 
