62ba5d011cd174a5620d6d76fbed2265d915d735
Lucía Costamagna <luci3220@hotmail.com>
2023-07-10 15:06:08 -0300

    add useContext

--- App.tsx
+++ App.tsx
@@ -2,15 +2,25 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/Navigator/Navigator';
+import { ThemeProvider } from './src/Context/ThemeContext';
 
 
 
 export default function App() {
   return (
+   <AppState>
     <NavigationContainer>
       <Navigator/>
     </NavigationContainer>
+    </AppState>
   );
 }
 
 
+const AppState=({children}:any)=>{
+  return(
+    <ThemeProvider>
+      {children}
+    </ThemeProvider>
+  )
+}
--- src/Components/Animation.tsx
+++ src/Components/Animation.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import {View} from "react-native";
+import { View } from "react-native";
 import LottieView from "lottie-react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { ThemeContext } from "../Context/ThemeContext";
+
 const Animation = () => {
+  const { setDarkTheme } = useContext(ThemeContext);
+
   return (
     <View>
-      <LottieView
-        autoPlay={true}
-        style={{
-          width: "10%",
-          backgroundColor: "transparent",
-          alignSelf: "center",
-        }}
-        source={require("../../assets/Animation/148518-dark-to-light-mode-switch-button.json")}
-      />
+      <TouchableOpacity onPress={setDarkTheme}>
+        <LottieView
+          autoPlay={true}
+          style={{
+            width: "10%",
+            backgroundColor: "transparent",
+            alignSelf: "center",
+          }}
+          source={require("../../assets/Animation/148518-dark-to-light-mode-switch-button.json")}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
